0dec95b2a1b7a84897eb0207db5b80587bfc8828
habibahassan <halimaadan92@gmail.com>
2020-01-12 01:31:58 +0300

    changed delete button on quote-detail.component.ts

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -10,10 +10,18 @@ export class QuotesDetailsComponent implements OnInit {
   @Input() quote: Quotes;
   @Output() deleteit = new EventEmitter<boolean>();
 
-  quoteDeleting(deleting: boolean) {
-    this.deleteit.emit(deleting);
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
   }
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}?`)
 
+      if (toDelete){
+        this.quote.splice(index,1)
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
